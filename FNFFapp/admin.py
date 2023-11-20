a188b3111417288aefaf2f1c5c6f745fb824bbcd
Ger-23 <>
2023-11-20 21:56:49 +0000

    Completed comment functionality

--- FNFFapp/admin.py
+++ FNFFapp/admin.py
@@ -7,8 +7,9 @@ from django_summernote.admin import SummernoteModelAdmin
 class PostAdmin(SummernoteModelAdmin):
 
     list_filter = ('status',  'created_on')
-    list_display = ('title', 'status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content')
 
 
@@ -17,7 +18,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'address', 'body')
-    actions = ['approved_comments']
+    actions = ['approve_comments']
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
