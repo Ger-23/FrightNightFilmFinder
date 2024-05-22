df8450615b0dd384f05d025c0f4ffac8e76817b6
Ger-23 <hardiman.g@outlook.com>
2024-05-22 14:19:10 +0000

    Ran tests for forms.py

--- FNFF/settings.py
+++ FNFF/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEV' in os.environ 
 
 X_FRAME_OPTIONS = 'SAME_ORIGIN'
 
@@ -105,18 +105,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FNFF.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herohuapp.com"
+]
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
-DATABASES = {
+if 'DEV' in os.environ:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
      'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
--- FNFFapp/test_forms.py
+++ FNFFapp/test_forms.py
@@ -1,1 +1,31 @@
 from django.test import TestCase
+from .models import Comment, Post
+from .forms import CommentForm, PostForm
+from django_summernote.widgets import SummernoteWidget
+
+
+class CommentFormTest(TestCase):
+    """ Test that form uses correct model and fields"""
+    
+    def test_form_model(self):
+        self.assertEqual(CommentForm._meta.model, Comment)
+
+    def test_form_fields(self):
+        self.assertEqual(CommentForm._meta.fields, ('body',))
+
+
+class PostFormTest(TestCase):
+    """ Test that form uses correct model and fields """
+    
+    def test_form_model(self):
+        self.assertEqual(PostForm._meta.model, Post)
+
+    def test_form_fields(self):
+        self.assertEqual(set(PostForm._meta.fields), set(('title', 'content', 'featured_image', 'excerpt')))
+
+    def test_content_widget(self):
+        self.assertIsInstance(PostForm._meta.widgets['content'], SummernoteWidget)
+
+    def test_excerpt_widget(self):
+        self.assertIsInstance(PostForm._meta.widgets['excerpt'], SummernoteWidget)
+
