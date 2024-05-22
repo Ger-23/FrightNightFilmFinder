7ec0f212e8c9e8374a48adad55aba652143212f2
Ger-23 <hardiman.g@outlook.com>
2024-05-22 21:43:48 +0000

    Ran tests for models.py

--- FNFFapp/test_models.py
+++ FNFFapp/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Post
+from django.utils import timezone
+from .models import Post, Comment, TeamMember
 
 
 class PostModelTest(TestCase):
@@ -41,6 +42,73 @@ class PostModelTest(TestCase):
 
     def test_number_of_likes(self):
         """ Test the number of likes method """
-        usertwo = User.objects.create_user(username='usertwo', password='pass2')
+        usertwo = User.objects.create_user(username='usertwo', password='passtwo')
         self.post.likes.add(usertwo)
         self.assertEqual(self.post.number_of_likes(),1)
+
+
+class CommentModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        """ Create a user, a post and a comment to test """
+        cls.user = User.objects.create_user(username='user', password='pass')
+        cls.post = Post.objects.create(title='Test Post', author=cls.user)
+        now = timezone.now()     
+        cls.comment1 = Comment.objects.create(
+            post=cls.post,
+            name='Jim Boy',
+            email='jim@thesite.com',
+            body='Second Comment.',
+            created_on=now,
+        )
+
+        cls.comment = Comment.objects.create(
+            post=cls.post,
+            name='John Boy',
+            email='jb@thesite.com',
+            body='Test Comment.',
+            created_on=now - timezone.timedelta(seconds=5)
+        )
+        
+    def test_comment_creation(self):
+        """ Test comment creation """
+        self.assertIsInstance(self.comment, Comment)
+        self.assertEqual(self.comment.post, self.post)
+        self.assertEqual(self.comment.name, 'John Boy')
+        self.assertEqual(self.comment.email, 'jb@thesite.com')
+        self.assertEqual(self.comment.body, 'Test Comment.')
+        self.assertIs(self.comment.approved, False)
+
+
+    def test_comment_str(self):
+        """ Test comment string """
+        self.assertEqual(
+            str(self.comment),
+            'Comment Test Comment. by John Boy'
+        )
+
+    def test_comment_order(self):
+        """ Test if comments are ordered by created_on (latest first) """
+        comments = self.post.comments.all()
+        self.assertEqual(comments[0], self.comment1)
+        self.assertEqual(comments[1], self.comment)
+
+
+class TeamMemberModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        """ Creat team member to test"""
+        cls.team_member = TeamMember.objects.create(
+            name='Josey Girl', bio='Big horror fan! Death to romcoms!!')
+
+    def test_team_member_creation(self):
+        """ Test team member creation """
+        self.assertIsInstance(self.team_member, TeamMember)
+        self.assertEqual(self.team_member.name, 'Josey Girl')
+        self.assertEqual(self.team_member.bio, 'Big horror fan! Death to romcoms!!')
+
+    def test_comment_str(self):
+        """ Test team_member string """
+        self.assertEqual(str(self.team_member),'Josey Girl')
--- FNFFapp/test_views.py
+++ FNFFapp/test_views.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Post
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class PostListTest(TestCase):
+
+    def __init__(self, *arg, **kwargs):
+        super().__init__(*arg, **kwargs)
+        self.user = kwargs.get('user')
+
+    def setUp(self):
+        if not self.user:
+            self.user = User.objects.create_user(username='user', password='pass')
+        self.published_post1 = Post.objects.create(title="Published Post 1", status=1, created_on='23-05-24', author=self.user)
+        self.published_post2 = Post.objects.create(title="Published Post 2", status=1, created_on='22-05-24', author=self.user)
+        self.draft_post = Post.objects.create(title="Draft Post", status=0, created_on='21-05-24', author=self.user)
+        self.client = Client()
+
+    def test_get_published_post(self):
+        """ Test that view returns published posts, ordered by creation date """
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        context = response.context
+        posts = context['object_list']
+        self.assertEqual(len(posts), 2)
+        self.assertEqual(posts[0].title, self.published_post2.title)
+        self.assertEqual(posts[1].title, self.published_post1.title)
+
